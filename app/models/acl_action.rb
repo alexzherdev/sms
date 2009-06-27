86f2f03a11e66e5d82263e68d4facede0e9fd3d0
alex.zherdev@gmail.com <alex@alex-zherdevs-macpro31.local>
2009-06-28 00:09:02 +0300

    Role associations

---
+++ app/models/acl_action.rb
@@ -0,0 +1,3 @@
+class AclAction < ActiveRecord::Base
+  has_and_belongs_to_many :roles
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
-class User < Person
+class User < Person  
+  has_and_belongs_to_many :roles
+  
   acts_as_authentic
+  
+  def can_access?(signature)
+    return self.roles.any? do |role|
+      role.acl_actions.any? { |act| act.name == signature }
+    end    
+  end
 end
