29664fff1aaf1324bb92727b76ee25401a1261df
Alex Zherdev <alex@alex-zherdevs-imac.local>
2010-02-20 01:00:06 +0200

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,33 @@ set :deploy_via, :remote_cache
 # your SCM below:
 set :repository, "git@github.com:alexzherdev/sms.git"
 set :scm, :git
+set :scm_command, "/usr/local/git/bin/git"
+set :local_scm_command, "/usr/local/git/bin/git"
 set :user, "alex"
 set :branch, "master"
+set :runner, "alex"
 
 set :rails_env, "production"
 
-role :app, "sms.kicks-ass.org"
-role :web, "sms.kicks-ass.org"
-role :db,  "sms.kicks-ass.org", :primary => true
+role :app, "localhost"
+role :web, "localhost"
+role :db,  "localhost", :primary => true
+
+task :after_update_code, :roles => [:app] do
+  run "ln -s /#{shared_path}/vendor #{release_path}/vendor"
+end
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
