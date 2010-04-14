c0f41da48c7341bbb291a8606566d78ca6339d33
Alex Zherdev <alex@alex-zherdevs-imac.local>
2010-04-14 20:21:00 +0300

    sphinx in deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,13 @@ role :app, "localhost"
 role :web, "localhost"
 role :db,  "localhost", :primary => true
 
+desc "Reconfigures and restarts Sphinx search daemon"
+task :restart_sphinx do
+  run "cd #{current_path}; rake ts:stop RAILS_ENV=production; true"
+  run "cd #{current_path}; rake ts:index RAILS_ENV=production"
+  run "cd #{current_path}; rake ts:start RAILS_ENV=production"
+end
+
 task :after_update_code, :roles => [:app] do
   run "ln -s /#{shared_path}/vendor #{release_path}/vendor"
 end
@@ -40,3 +47,5 @@ namespace :deploy do
     run "touch #{current_release}/tmp/restart.txt"
   end
 end
+
+after "deploy:restart", :restart_sphinx
