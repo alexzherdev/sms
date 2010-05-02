1490183e5927be78b59a01a03ab5ea611b7e49c5
Alex Zherdev <alex@alex-zherdevs-imac.local>
2010-05-02 21:24:50 +0300

    deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,12 @@
 set :application, "SMS"
 
+set :default_environment, { 
+  'PATH' => "/Users/alex/.rvm/ree-1.8.7-2009.10/bin:/Users/alex/.rvm/gems/ree-1.8.7-2009.10/bin:/Users/alex/.rvm/bin:$PATH",
+  'RUBY_VERSION' => '1.8.7',
+  'GEM_HOME'     => '/Users/alex/.rvm/gems/ree-1.8.7-2009.10',
+  'GEM_PATH'     => '/Users/alex/.rvm/gems/ree-1.8.7-2009.10'
+}
+
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
@@ -30,12 +37,12 @@ task :restart_sphinx do
 end
 
 task :after_update_code, :roles => [:app] do
-  run "ln -s /#{shared_path}/vendor #{release_path}/vendor"
+  run "ln -s /#{shared_path}/vendor/rails #{release_path}/vendor/rails"
 end
 
 namespace :deploy do
   task :migrate, :roles => :app do
-    run "cd #{current_path}; rvm rake db:migrate RAILS_ENV=production"
+    run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
   end
   
   task :start, :roles => :app do
